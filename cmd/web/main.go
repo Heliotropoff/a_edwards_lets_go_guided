de7f865507431967ed01190b391e08219cfd39df
Gregory Yevgraphov <evgrafovgregory@gmail.com>
2026-06-02 20:28:10 +0200

    complete chapter 2, add basic handlers and static files

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -5,18 +5,17 @@ import (
 	"net/http"
 )
 
-// Define a home handler function which writes a byte slice containing
-// "Hello from Snippetbox" as the response body.
-func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello from Snippetbox"))
-}
-
 func main() {
 	// Use the httpNewServeMux() to initialize a new servemux
 	//register the home function as the handler  for the "/" URL pattern
 
 	mux := http.NewServeMux()
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 	mux.HandleFunc("/", home)
+	//register new handlers
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	// Use the http.ListenAndServe() function to start a new web server
 	// We pass in the TCP network address to listenn on and servemux
